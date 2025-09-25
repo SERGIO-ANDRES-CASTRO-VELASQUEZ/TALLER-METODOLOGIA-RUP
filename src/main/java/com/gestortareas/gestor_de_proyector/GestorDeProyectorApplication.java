1139a3f108a403c8c3187bc09966fa2c1626d8f1
SERGIO-ANDRES-CASTRO-VELASQUEZ <sergio_castrove@fet.edu.co>
2025-09-25 17:06:45 -0500

    Creacion del repositorio

---
+++ src/main/java/com/gestortareas/gestor_de_proyector/GestorDeProyectorApplication.java
@@ -0,0 +1,13 @@
+package com.gestortareas.gestor_de_proyector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestorDeProyectorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestorDeProyectorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gestortareas/gestor_de_proyector/GestorDeProyectorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestortareas.gestor_de_proyector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestorDeProyectorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
