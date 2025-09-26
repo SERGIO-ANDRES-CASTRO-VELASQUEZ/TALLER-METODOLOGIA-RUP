8bd50b58fa782c6a5b6335c4813df2c466ab6b59
SERGIO-ANDRES-CASTRO-VELASQUEZ <sergio_castrove@fet.edu.co>
2025-09-25 23:30:26 -0500

    Creacion Capa Repositorio

---
+++ src/main/java/com/gestortareas/gestor_de_proyector/repository/EstudianteRepo.java
@@ -0,0 +1,8 @@
+package com.gestortareas.gestor_de_proyector.repository;
+
+import com.gestortareas.gestor_de_proyector.entity.Estudiante;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EstudianteRepo extends JpaRepository<Estudiante, Long> {
+
+}
---
+++ src/main/java/com/gestortareas/gestor_de_proyector/repository/ProyectoRepo.java
@@ -0,0 +1,8 @@
+package com.gestortareas.gestor_de_proyector.repository;
+
+import com.gestortareas.gestor_de_proyector.entity.Proyecto;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProyectoRepo extends JpaRepository<Proyecto, Long> {
+
+}
